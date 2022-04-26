c3914e44caac0f0eb6e7157d75e7f38a6b7eaedf
shaanku01 <shankersai.gr@zemosolabs.com>
2022-04-26 16:47:43 +0530

    FC

---
+++ src/main/java/com/zemoso/springboot/crud/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.zemoso.springboot.crud.dao;
+
+import com.zemoso.springboot.crud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+}
